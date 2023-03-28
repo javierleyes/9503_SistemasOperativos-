e5e4df0dffbd400e64b629c087248a971b962a36
Javier <javier.nunezleyes@gmail.com>
2023-03-27 23:10:41 -0300

    fork and pipe

--- Labs/Primes/primes.c
+++ Labs/Primes/primes.c
@@ -27,10 +27,73 @@ int getNumberToCalculatePrimes(char *number)
 
 int main(int argc, char *argv[])
 {
-
     int receivedNumber = getNumberToCalculatePrimes(argv[1]);
+    int numberCandidates = receivedNumber - 1;
 
     printf("The received number is %d\n", receivedNumber);
+
+    printf("Generating an array with number to check...\n");
+   
+    printf("Done\n");
+
+    // Parent pipe.
+    int parentToChildPipe[2];
+
+    if (pipe(parentToChildPipe) < 0)
+    {
+        perror("Pipe error");
+        exit(-1);
+    }
+
+    int forkResult = fork();
+    if (forkResult == -1)
+    {
+        perror("Fork error");
+        exit(-1);
+    }
+
+    // Parent.
+    if (forkResult != 0) 
+    {
+        close(parentToChildPipe[0]);
+
+        int primesNumberCandidates[numberCandidates];
+
+        int firstPrimesNumber = 2;
+        for (int i = 0; i < numberCandidates; i++)
+        {
+            primesNumberCandidates[i] = firstPrimesNumber++;
+        }
+
+        printf("\nIam the parent\n");
+
+        write(parentToChildPipe[1], &primesNumberCandidates, sizeof(primesNumberCandidates));
+
+        close(parentToChildPipe[1]);
+
+        wait(NULL);
+    } 
+    else 
+    {
+        close(parentToChildPipe[1]);
+
+        int primesNumber[numberCandidates];
+        read(parentToChildPipe[0], &primesNumber, sizeof(primesNumber));
+
+        printf("\nIam the child\n");
+        printf("I received the numbers:\n");
+        for (int i = 0; i < numberCandidates; i++)
+        {
+            printf("%d, ", primesNumber[i]);
+        }
+
+        close(parentToChildPipe[0]);
+
+        exit(0);
+    }
+    
+    
+    printf("\nEnd\n");
 
     //   int i = fork();
 
